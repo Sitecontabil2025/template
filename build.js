3157160e55d8921fd2e4bfd7f07ed4e1088e188e
Sitecontabil <carlos@sitecontabil.com.br>
2025-11-26 14:26:20 -0300

    Update FontAwesome version and add jQuery confirm

--- build.js
+++ build.js
@@ -88,12 +88,12 @@ const fileContents = {
     <i class="fab fa-whatsapp"></i>
 </a>
 
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.7.1/css/all.css">
-<!-- <script src="assets/js/cookie.min.js" data-position="left" data-hide="true" data-cor="var(--bs-primary)"></script> -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v7.1.0/css/all.css">
+<script src="assets/js/cookie.min.js" data-position="left" data-hide="true" data-cor="var(--bs-primary)"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/swiper-bundle.min.js"></script>
 <script src="assets/js/aos.js"></script>
-<script src="assets/js/sweetalert2.all.min.js"></script>
+<script src="assets/js/jquery-confirm.min.js"></script>
 <script src="assets/js/script.min.js"></script>
 
 </body>
