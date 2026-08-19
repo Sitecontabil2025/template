bf28b6b1a9a2f6955d0ece47e612cf188c1cb128
Sitecontabil <carlos@sitecontabil.com.br>
2026-08-19 11:31:39 -0300

    Refactor build.js by removing enviar.php content
    Removed 'enviar.php' content and updated file list in build.js.

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ folders.forEach(folder => {
     }
 });
 
-const files = ["header.php", "index.php", "footer.php", "dados.php", "enviar.php"];
+const files = ["header.php", "index.php", "footer.php", "dados.php"];
 
 const [
     escritorio, endereco, numero, complemento, bairro, cidade, estado, cep, mapaLink, mapa,
@@ -80,7 +80,6 @@ const fileContents = {
     <meta name="resource-type" content="website" />
 
     <!-- ARQUIVOS CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/css/jquery-confirm.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/aos.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/swiper-bundle.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.min.css?v=' . time()) ?>">
@@ -259,194 +258,6 @@ function base_url($path = '')
 
     return $protocol . $host . $pathBase . ltrim($path, '/');
 }
-`,
-    "enviar.php": `<?php
-// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-
-ob_start();
-session_start();
-
-
-if (empty($_POST['g-recaptcha-response'])):
-    $retorno = "";
-    $retorno .= "O campo <strong>Re'Captcha</strong> é de preenchimento obrigatório.<br>";
-    $resposta = array('tipo' => 'orange', 'mensagem' => $retorno);
-    echo json_encode($resposta);
-else:
-
-    $captcha = $_POST['g-recaptcha-response'];
-
-    $secret_key = '6LenHswpAAAAANV2onVScenPGA_y9jOOmnw2h8F-';
-    $response = $captcha;
-    // $remoteip = $_SERVER['REMOTE_ADDR'];
-    $remoteip = $_SERVER['HTTP_CLIENT_IP'];
-
-    $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secret_key . '&response=' . $response . '&remoteip=' . $remoteip;
-    $file = file_get_contents($url);
-    $data = json_decode($file);
-
-    if ($data->success != '1'):
-
-        $retorno = "";
-        $retorno .= "O campo <strong>Re'Captcha</strong> foi negado.<br>";
-        $resposta = array('tipo' => 'orange', 'mensagem' => $retorno);
-        echo json_encode($resposta);
-
-    else:
-        // Pegando os dados do formulário de contato
-        $formnome = $_POST['nome'];
-        $formemail = $_POST['email'];
-        $formcelular = $_POST['celular'];
-        $formmensagem = $_POST['mensagem'];
-
-        // verificando se os campos foram todos digitados
-        if (empty($formnome) || empty($formemail) || empty($formcelular) || empty($formmensagem)) :
-            $campos = array();
-            if (empty($formnome)) $campos[] = "nome";
-            if (empty($formemail)) $campos[] = "email";
-            if (empty($formcelular)) $campos[] = "celular";
-            if (empty($formmensagem)) $campos[] = "mensagem";
-
-            $retorno = "";
-            foreach ($campos as $campo):
-                $retorno .= "O campo <strong>$campo</strong> é de preenchimento obrigatório.<br>";
-            endforeach;
-
-            // passando o tipo do alerta e a mensagem
-            $resposta = array('tipo' => 'orange', 'mensagem' => $retorno);
-            echo json_encode($resposta);
-        elseif (!filter_var($formemail, FILTER_VALIDATE_EMAIL)) :
-            // passando o tipo do alerta e a mensagem
-            $resposta = array('tipo' => 'orange', 'mensagem' => 'O e-mail inserido não é válido.');
-            echo json_encode($resposta);
-        else :
-            // Não esquecer de alterar esta linha com o domínio do cliente
-            $site = "https://${data.dominio}"; // URL completa do site com o http:// ou https://
-
-            // E-mail que irá receber o formulário
-            $destinatario = "${data.email}";
-
-            // Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
-            require_once("phpmailer/PHPMailerAutoload.php");
-
-            // Inicia a classe PHPMailer
-            $mail = new PHPMailer();
-
-            // SMTPDebug 0, 1, 2 e 3
-            $mail->SMTPDebug = 0;
-
-            // Conexões menos seguras
-            $mail->SMTPOptions = array(
-                'ssl' => array(
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true
-                )
-            );
-
-            // Define os dados do servidor e tipo de conexão
-            $mail->IsSMTP(); // Define que a mensagem será SMTP
-            $mail->Host = "mail.${data.dominio}"; // Endereço do servidor SMTP
-            $mail->SMTPAuth = true; // Usa autenticação SMTP?
-
-            // Usuário do servidor SMTP
-            $mail->Username = 'formulario@${data.dominio}';
-
-            // Senha do servidor SMTP
-            $mail->Password = 'email@1234';
-
-            // Caso o servidor use uma porta e uma autenticação diferente
-            $mail->Port = 587;
-            // $mail->SMTPSecure = 'ssl';
-            // $mail->SMTPSecure = "tls";
-            $mail->SMTPAutoTLS = false;
-
-            // Define o remetente
-            $mail->setFrom('formulario@${data.dominio}'); // E-mail do destinatário
-
-            // Copia Oculta
-            // $mail->AddBCC("");
-
-            // Define os destinatário(s) (Tem de estar no mesmo servidor)
-            $mail->AddAddress($destinatario); // Quem vai receber o e-mail
-            $mail->AddReplyTo($formemail);
-
-            // Define os dados técnicos da Mensagem
-            $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-            $mail->CharSet = 'utf-8'; // Charset da mensagem
-            //$mail->CharSet = 'iso-8859-1'; // Charset da mensagem
-
-            // Define os anexos (Basta colocar o caminho do arquivo)
-            //$mail->AddAttachment('caminho-do-arquivo.txt');
-
-            date_default_timezone_set('America/Sao_Paulo');
-            if (date('H') > 0 && date('H') < 12):
-                $saudacao = "Bom dia";
-            elseif (date('H') >= 12 && date('H') < 18):
-                $saudacao = "Boa tarde";
-            else:
-                $saudacao = "Boa noite";
-            endif;
-
-            // Escrevendo a mensagem do e-mail
-            $mensagem = "<html>";
-            $mensagem .= "<head>";
-            $mensagem .= "<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />";
-            $mensagem .= "<title></title>";
-            $mensagem .= "</head>";
-            $mensagem .= "<body style='font-size:16px; background-color:#f7f7f7; margin:0; padding:0; overflow:hidden;'>";
-            $mensagem .= "<div style='font-family:sans-serif; width:610px; padding:15px; border-radius:5px; background-color:#fff; margin-top:30px; margin-bottom:30px; margin-left:auto; margin-right:auto;'>";
-            $mensagem .= "<h1 style='font-size:20px; margin-top:0;'>E-MAIL ENVIADO DE UM FORMULÁRIO DE CONTATO</h1>";
-            $mensagem .= "<hr>";
-            $mensagem .= "<p>$saudacao,</p>
-                <p>Foi preenchido um novo formulário de contato no site <a href='$site'>$site</a>.</p>
-                <p><strong>Nome:</strong> $formnome<br>
-                <strong>E-mail:</strong> $formemail<br>
-                <strong>Celular:</strong> $formcelular<br></p>
-                <p>
-                $formmensagem</p>";
-            $mensagem .= "<hr>";
-            $mensagem .= "<p style='margin:0; font-size:14px;'>Este e-mail foi enviado pelo formulário de contato do site <a href='$site'>$site</a>.</p>";
-            $mensagem .= "</div>";
-            $mensagem .= "</body>";
-            $mensagem .= "</html>";
-
-            // Escrevendo a mensagem alternativa caso o cliente de e-mail não suporte html
-            $mensagem2 = "E-MAIL ENVIADO DE UM FORMULÁRIO DE CONTATO\r\n \r\n";
-            $mensagem2 .= "$saudacao,\r\n \r\n
-                Foi preenchido um novo formulário de contato no site $site.\r\n \r\n
-                Nome: $formnome\r\n
-                E-mail: $formemail\r\n
-                Celular: $formcelular\r\n;
-                Mensagem enviada:\r\n
-                $formmensagem\r\n \r\n
-                Este e-mail foi enviado pelo formulário de contato do site $site.";
-
-            // Define a mensagem (Texto e Assunto)
-            $mail->Subject = "FORMULÁRIO DE CONTATO"; // Assunto da mensagem
-            $mail->Body = $mensagem;
-            $mail->AltBody = $mensagem2;
-
-            // Envia o e-mail
-            $enviado = $mail->Send();
-
-            // Limpa os destinatários e os anexos
-            $mail->ClearAllRecipients();
-            $mail->ClearAttachments();
-
-            // verifica se o e-mail foi enviado
-            if ($enviado) :
-                // passando o tipo do alerta e a mensagem
-                $resposta = array('tipo' => 'green', 'mensagem' => 'Seu e-mail foi enviado com sucesso.');
-                echo json_encode($resposta);
-            else :
-                // passando o tipo do alerta e a mensagem
-                $resposta = array('tipo' => 'red', 'mensagem' => 'Seu e-mail não pode ser enviado. Erro: ' . $mail->ErrorInfo);
-                echo json_encode($resposta);
-            endif;
-        endif;
-    endif;
-endif;
 `
 };
 
@@ -574,84 +385,7 @@ function copyFile(src, dest) {
 
 const scriptJsPath = path.join(projectRoot, "assets/js", "script.js");
 
-const scriptJsContent = `$(document).ready(function () {
-    var SPMaskBehavior = function (val) {
-        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-    },
-        spOptions = {
-            onKeyPress: function (val, e, field, options) {
-                field.mask(SPMaskBehavior.apply({}, arguments), options);
-            }
-        };
-    $('.celular-mask').mask(SPMaskBehavior, spOptions);
-});
-
-$loading = $.dialog({
-    content: "Enviando sua mensagem",
-    title: false,
-    type: "green",
-    theme: "modern",
-    lazyOpen: true,
-    closeIcon: false,
-    icon: "fas fa-circle-notch fa-spin",
-    buttons: false,
-});
-
-$('#formcontato').on('submit', function (e) {
-    e.preventDefault();
-    var dados = $(this).serialize();
-
-    $.ajax({
-        type: "POST",
-        url: "enviar.php",
-        data: dados,
-        dataType: 'json',
-        beforeSend: function () {
-            $loading.open();
-        },
-        success: function (resposta) {
-            if (resposta.tipo == "green") {
-                $icone = "far fa-check-circle";
-            } else {
-                $icone = "fas fa-times";
-            }
-            $.alert({
-                content: resposta.mensagem,
-                title: false,
-                type: resposta.tipo,
-                theme: "modern",
-                closeIcon: false,
-                icon: $icone,
-                buttons: {
-                    ok: function (okButton) {
-                        if (resposta.tipo == "green") {
-                            location.href = "https://${data.dominio}"
-                        }
-                    }
-                }
-            });
-        },
-        error: function (resposta) {
-            $.alert({
-                content: "Um erro desconhecido aconteceu e sua mensagem não pode ser enviada",
-                title: false,
-                type: "red",
-                theme: "modern",
-                closeIcon: true,
-                icon: "fas fa-times",
-                buttons: {
-                    ok: function () {
-                    }
-                }
-            });
-        },
-        complete: function () {
-            $loading.close()
-        }
-    });
-    return false;
-});
-`;
+const scriptJsContent = ``;
 
 if (!fs.existsSync(scriptJsPath)) {
     fs.writeFileSync(scriptJsPath, scriptJsContent);
@@ -667,9 +401,6 @@ const assets = [
     { src: "node_modules/swiper/swiper-bundle.min.js", dest: "assets/js/swiper-bundle.min.js" },
     { src: "node_modules/aos/dist/aos.css", dest: "assets/css/aos.css" },
     { src: "node_modules/aos/dist/aos.js", dest: "assets/js/aos.js" },
-    { src: "node_modules/jquery-confirm/dist/jquery-confirm.min.js", dest: "assets/js/jquery-confirm.min.js" },
-    { src: "node_modules/jquery-confirm/dist/jquery-confirm.min.css", dest: "assets/css/jquery-confirm.min.css" },
-    { src: "node_modules/cookie/cookie.min.js", dest: "assets/js/cookie.min.js" }
 ];
 
 assets.forEach(asset => {
